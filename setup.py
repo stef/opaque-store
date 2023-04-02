3ca9f3f253eb97b74348fcedce96ba3c25714caf
stf <7o5rfu92t@ctrlc.hu>
2023-04-02 03:33:54 +0200

    [mod] bumped version to 0.0.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name = 'opaquestore',
-      version = '0.0.4',
+      version = '0.0.5',
       description = 'Simple Online secret-storage based on the OPAQUE protocol',
       license = "GPLv3",
       author = 'Stefan Marsiske',
